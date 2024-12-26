398ab073dd76758e3b4aa0d130cf5a6fbe549626
REDfriend <83909061+1REDfriend@users.noreply.github.com>
2024-12-26 11:52:43 +0700

    Update Lab 02.02 - Student Groups.py

--- LAB02/Lab 02.02 - Student Groups.py
+++ LAB02/Lab 02.02 - Student Groups.py
@@ -1,18 +1,83 @@
-class Student :
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Students:
     def __init__(self):
-        self.data = list()
-    
-    def push(self, data) :
-        self.data.append(data)
+        self.head = None
 
-    def pop(self, data) :
-        if self.data :
-            self.data.pop()
+    def push(self, value=""):
+        newNode = Node(value)
+        if not self.head:
+            self.head = newNode
+        else:
+            currentNode = self.head
+            while currentNode.next:
+                currentNode = currentNode.next
+            currentNode.next = newNode
 
-def main() :
-    students = list()
-    student = Student
+    def pop(self):
+        if not self.head:
+            return None
+        if not self.head.next:
+            temp_node = self.head
+            self.head = None
+            return temp_node
+        currentNode = self.head
+        prevNode = None
+        while currentNode.next:
+            prevNode = currentNode
+            currentNode = currentNode.next
+        prevNode.next = None
+        return currentNode
 
-    m = int(input())
-    n = int(input())
-    
+    def pop_first(self):
+        if not self.head:
+            return None
+        current_node = self.head
+        self.head = self.head.next
+        current_node.next = None
+        return current_node
+
+    def is_empty(self):
+        return self.head is None
+
+    def show_detail(self):
+        currentNode = self.head
+        result = ""
+        while currentNode:
+            result += currentNode.data
+            if currentNode.next:
+                result += ", "
+            currentNode = currentNode.next
+        return result
+
+
+if __name__ == "__main__":
+    groups = Students()
+    nameList = Students()
+
+    groupCount = int(input())
+    allStudentCount = int(input())
+
+    for _ in range(groupCount):
+        groups.push(Students())
+
+    for _ in range(allStudentCount):
+        nameList.push(input())
+
+    current_group = groups.head
+    while not nameList.is_empty():
+        if not current_group:
+            current_group = groups.head
+        current_group.data.push(nameList.pop().data)
+        current_group = current_group.next
+
+    current_group = groups.head
+    group_number = 1
+    while current_group:
+        print(f"Group {group_number}: {current_group.data.show_detail()}")
+        group_number += 1
+        current_group = current_group.next
